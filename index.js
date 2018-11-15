2357d904acc55ef5f30a2e492e9c811b530167c2
weijun huang <luck938938@hotmail.com>
2018-11-15 18:47:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const getFirstSelector = (selector) => {
+  return document.querySelector(selector)
 
+}
+
+const nestedTarget =() =>{
+  return document.getElementById('nested').querySelectorAll('div')[3]
+ 
+}
+
+const increaseRankBy =(n)=>{
+    var upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
+    for(let i =0; i<upRank.length; i++){
+      upRank[i].innerHTML = parseInt(upRank[i].innerHTML+ n)
+    }
+}
+
+const deepestChild =()=>{
+  return document.getElementById('grand-node').querySelectorAll('div')[3]
+}
