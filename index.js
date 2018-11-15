2c13bc4daa812c3c0388b24c94a563b0cd8d9afd
weijun huang <luck938938@hotmail.com>
2018-11-15 18:17:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const getFirstSelector = (selector) => {
+   var firstSelector= document.querySelector(selector)
+    return firstSelector;
+}
 
+const nestedTarget =() =>{
+  var result = document.getElementById('nested').querySelectorAll('div')[3]
+  return result;
+}
