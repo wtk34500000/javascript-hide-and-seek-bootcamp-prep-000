aaf4b11ce7408e939538023b0fbc4e86e3283bf1
weijun huang <luck938938@hotmail.com>
2018-11-15 18:41:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const nestedTarget =() =>{
 
 const increaseRankBy =(n)=>{
   let upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
-  upRandk.forEach(function(rank){
+  upRand.forEach(function(rank){
     rank.innerHTML = parseInt(rank.innerHTML+n)
     
   })
